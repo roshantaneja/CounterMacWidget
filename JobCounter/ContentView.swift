e197b2a507c36a1552ca02504fb319db589c2e44
Smriti Rajan <smritirajan@mac.lan>
2026-08-09 15:22:41 -0400

    Initialize MacOS XCode set up

---
+++ JobCounter/ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Job Counter Competition")
+            .font(.largeTitle)
+            .padding()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+#Preview {
+    ContentView()
+}
